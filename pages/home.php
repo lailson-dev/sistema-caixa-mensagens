1cfef6da3eaa87cd8c074a84bdd0e15b1c353944
Lailson Conceição <lailsoncv@hotmail.com>
2019-01-21 14:32:51 -0200

    Criação das páginas de listagem e visualização dos emails

---
+++ pages/home.php
@@ -0,0 +1,23 @@
+<h1 class="text-center">Emails Recebidos</h1>
+<form action="" method="post">
+	<table class="table table-inverse">
+	<tbody>
+		<tr>
+			<td><strong><input class="btn btn-info" type="submit" name="delete" value="Apagar"></td></strong>
+			<td><strong>Nome</strong></td>
+			<td><strong>Email</strong></td>
+			<td><strong>Assunto</strong></td>
+			<td><strong>Data</strong></td>
+			<td><strong>Visualizar</strong></td>
+		</tr>
+		<tr>
+			<td><input type="checkbox" name="id[]" value=""></td>
+			<td>Nome</td>
+			<td>Email</td>
+			<td>Assunto</td>
+			<td>Data</td>
+			<td>Visualizar</td>
+		</tr>
+	</tbody>
+</table>
+</form>
---
+++ pages/show.php
@@ -0,0 +1,18 @@
+<h1 class="text-center">Fulano de Tal</h1>
+<span class="d-block lead"><strong>De:</strong> Lailson Conceição</span>
+<span class="d-block lead"><strong>Email:</strong> lailson@lailson.com.br</span>
+<span class="d-block lead"><strong>Assunto:</strong> Pedidos</span>
+<span class="d-block lead"><strong>Data:</strong> 21/01/2019</span>
+<p class="lead bg-light mt-3 py-2">
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus beatae, nostrum cumque esse ratione maxime voluptatum suscipit incidunt, maiores nesciunt obcaecati, facere debitis laborum eius. Dolore sequi nemo dignissimos animi.
+</p>
+
+<form action="" method="post">
+	<div class="form-group">
+		<label for="message" class="form-control-label">Responder</label>
+    	<textarea class="form-control" id="message" name="message" rows="3"></textarea>
+	</div>
+	<div class="form-group">
+		<button type="submit" name="send" class="btn btn-outline-primary">Enviar</button>
+	</div>
+</form>
